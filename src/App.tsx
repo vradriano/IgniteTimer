6d34da07aca80bf051366b884fec9119d8b71def
unknown <vradriano@hotmail.com>
2022-09-18 11:18:30 -0300

    theme type - styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Button } from "./components/Button";
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
 
   return (
-   <>
+   <ThemeProvider theme={defaultTheme}>
     <Button />
-   </>
+   </ThemeProvider>
   )
 }
