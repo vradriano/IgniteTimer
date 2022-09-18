9f8692e4b82fbcb4723fc5558598efcaff6a26aa
unknown <vradriano@hotmail.com>
2022-09-18 14:57:54 -0300

    Header styles

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,4 +1,4 @@
-import { Header } from "../../components/Header";
+import { Header } from "../../components/Header/Header";
 import { Outlet } from "react-router-dom";
 import { LayoutContainer } from "./styles";
 
