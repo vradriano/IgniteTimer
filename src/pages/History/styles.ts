399c071a4e2a6855a6a2215ab7d6ddddacf3e1ac
unknown <vradriano@hotmail.com>
2022-09-19 21:49:54 -0300

    Status component - Important

--- src/pages/History/styles.ts
+++ src/pages/History/styles.ts
@@ -9,7 +9,7 @@ export const HistoryContainer = styled.main`
 
   h1 {
     font-size: 1.5rem;
-    color: ${props => props.theme['gray-100']}
+    color: ${(props) => props.theme['gray-100']};
   }
 `
 
@@ -26,7 +26,7 @@ export const HistoryList = styled.div`
     th {
       background-color: ${(props) => props.theme['gray-600']};
       padding: 1rem;
-      text-align: center;
+      text-align: left;
       color: ${(props) => props.theme['gray-100']};
       font-size: 0.875rem;
       line-height: 1.6;
@@ -36,29 +36,50 @@ export const HistoryList = styled.div`
         padding-left: 1.5rem;
       }
 
-      
       &:last-child {
         border-top-right-radius: 8px;
         padding-right: 1.5rem;
       }
+    }
 
-      td {
-        background-color: ${(props) => props.theme['gray-700']};
-        border-top: 4px solid ${(props) => props.theme['gray-800']};
-        padding: 1rem;
-        font-size: 0.875rem;
-        line-height: 1.6;
+    td {
+      background-color: ${(props) => props.theme['gray-700']};
+      border-top: 4px solid ${(props) => props.theme['gray-800']};
+      padding: 1rem;
+      font-size: 0.875rem;
+      line-height: 1.6;
 
-        &:first-child {
+      &:first-child {
         width: 50%;
         padding-left: 1.5rem;
-        }
+      }
 
-        
-        &:last-child {
-          padding-right: 1.5rem;
-        }
+      &:last-child {
+        padding-right: 1.5rem;
       }
     }
+  }
+`
+const STATUS_COLORS = {
+  yellow: 'yellow-500',
+  green: 'green-500',
+  red: 'red-500'
+} as const
+
+interface StatusProps {
+  statusColor: keyof typeof STATUS_COLORS
+}
+
+export const Status = styled.span<StatusProps>`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  &::before {
+    content: '';
+    width: 0.5rem;
+    height: 0.5rem;
+    border-radius: 50%;
+    background: ${(props) => props.theme[STATUS_COLORS[props.statusColor]]}
   }
 `
