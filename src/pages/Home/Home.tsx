bd3da2f6737381a523e36f27513b7f142b80f7b7
unknown <vradriano@hotmail.com>
2022-09-24 09:45:17 -0300

    Form typescript

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -20,12 +20,23 @@ const newCycleFormValidationSchema = zod.object({
     .max(60)
 })
 
+// interface NewCycleFormData {
+//   task: string;
+//   minutesAmount: number;
+// }
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
 export function Home() {
-  const { register, watch, handleSubmit } = useForm({
-    resolver: zodResolver(newCycleFormValidationSchema)
+  const { register, watch, handleSubmit } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0
+    }
   })
 
-  function handleCreateNewCycle(data: any) {
+  function handleCreateNewCycle(data: NewCycleFormData) {
 
   }
 
