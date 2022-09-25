43057adde71cf637093fb08f063f131db030eb3a
unknown <vradriano@hotmail.com>
2022-09-25 16:58:48 -0300

    Fix bugs, dinamyc title

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -46,12 +46,19 @@ export function Home() {
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
 
   useEffect(() => {
+    let interval: number;
+
     if(activeCycle) {
-      setInterval(() => {
+      interval = setInterval(() => {
         setAmountSecondsPast(
           differenceInSeconds(new Date(), activeCycle.startDate)
         )
       }, 1000)
+    }
+
+
+    return () => {
+      clearInterval(interval)
     }
   }, [activeCycle])
 
@@ -67,6 +74,7 @@ export function Home() {
 
     setCycles((prevProps) => [...prevProps, newCycle])
     setActiveCycleId(id)
+    setAmountSecondsPast(0)
     reset()
   }
 
@@ -78,6 +86,12 @@ export function Home() {
   
   const minutes = String(minutesAmount).padStart(2, '0')
   const seconds = String(secondsAmount).padStart(2, '0')
+
+  useEffect(() => {
+    if(activeCycle) {
+      document.title = `${minutes}:${seconds}`
+    }
+  }, [minutes, seconds, activeCycle])
 
   const task = watch('task')
   const isSubmitDisabled = !task
